4f7b98e6680b9171a14dd25c7806f25534ef4a33
Federico <federicofigini90@gmail.com>
2021-04-08 16:33:40 +0200

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,23 @@ let app = new Vue({
     el: "#root",
     data: {
         emails: [],
-        quantity: null
+        quantity: null,
+        errore: ""
     },
     methods: {
         generateEmail() {
             this.emails = []
-            for (let i = 0; i < this.quantity; i++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then((response) => {
-                        this.emails.push(response.data.response)
-                    });
+            if (isNaN(this.quantity) || this.quantity == null || this.quantity == "") {
+                this.errore = "NON HAI INSERITO UN NUMERO"
+            } else {
+                this.errore = ""
+                for (let i = 0; i < this.quantity; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((response) => {
+                            this.emails.push(response.data.response)
+                        });
+                }
+
             }
         }
     },
