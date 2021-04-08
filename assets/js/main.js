30e5a7038cc233b5b3a98699ddd6490985984106
Federico <federicofigini90@gmail.com>
2021-04-08 15:25:32 +0200

    finito

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+    el: "#root",
+    data: {
+        emails: []
+    },
+    methods: {
+        pippo() {
+
+        }
+
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emails.push(response.data.response)
+                });
+        }
+    },
+})
